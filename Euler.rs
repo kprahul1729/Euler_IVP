fc9ee224b21ecfdef43bfa12f22441c87454e962
kprahul1729 <kprahul1729@gmail.com>
2025-06-30 15:18:14 +0530

    Update Euler.rs
    error correction

--- Euler.rs
+++ Euler.rs
@@ -8,7 +8,7 @@ fn euler( f: impl Fn(f64, f64) -> f64,
   let mut result = Vec::with_capacity(n+1);
   result.push((t_start, y0));
   let mut y = y0;
-  let mut t = t_start;
+  let t = t_start;
   for _ in 0..n{
     y += h * f(t,y);
     result.push((t,y));

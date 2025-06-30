97ae0c5943ca16f2ff79c6e93471213ded922b97
kprahul1729 <kprahul1729@gmail.com>
2025-06-30 06:17:13 +0000

    error correction

--- Euler.py
+++ Euler.py
@@ -1,6 +1,6 @@
 def euler(f, Y0, a, b, n):
   h = (b - a) / n
-  y = [y0]
+  y = [Y0]
   t = a
   for _ in range(n):
     y_new = y[-1] + h * f(t, y[-1])
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Euler import euler
+from Euler import euler 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,10 +7,10 @@ def f(t, y):
   return math.cos(t) - y
 
 step_sizes = {20, 100, 1000}
-for n in step_sizes:
+for n in step_sizes:                
   y_values = euler(f, 1, 0, 5, n)
   t_values = np.linspace(0, 5, n + 1)
-  plt.plot(t_values, y_values, label='n='n)
+  plt.plot(t_values, y_values, label='n={n}')
   
 plt.xlabel('t')
 plt.ylabel('y')

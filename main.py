389a3c42119837314c916a3d7d3bdb626d2edd5a
kprahul1729 <kprahul1729@gmail.com>
2025-06-30 14:33:51 +0530

    Update main.py
    Added analytical solution to the plot

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ for n in step_sizes:
   t_values = np.linspace(0, 5, n + 1)
   plt.plot(t_values, y_values, label=f'n={n}')
   
+t_values = np.linspace(0, 5, 1001)
+y_analytical = 1/2*(np.sin(t_values) + np.cos(t_values)+np.exp(-1*t_values))
+plt.plot(t_values, y_analytical, label='Analytical Solution')
+
 plt.xlabel('t')
 plt.ylabel('y')
-plt.title('Euler Method for different n')
+plt.title('Euler Method for different n vs Analytical Solution')
 plt.legend()
 plt.grid(True)
 plt.show()

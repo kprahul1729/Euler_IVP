d6b4b7f8c105b5dd322be60f9cc6e0410daca38a
kprahul1729 <kprahul1729@gmail.com>
2025-06-30 11:16:07 +0530

    Update main.py
    added for loop to compute solutions for different time intervals and plot those values

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from Euler import euler
 import math
 import numpy as np
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def f(t, y):
   return math.cos(t) - y
+
+step_sizes = {20, 100, 1000}
+for n in step_sizes:
+  y_values = euler(f, 1, 0, 5, n)
+  t_values = np.linspace(0, 5, n + 1)
+  plt.plot(t_values, y_values, label='n='n)
+  
+plt.xlabel('t')
+plt.ylabel('y')
+plt.title('Euler Method for different n')
+plt.legend()
+plt.grid(True)
+plt.show()
 
